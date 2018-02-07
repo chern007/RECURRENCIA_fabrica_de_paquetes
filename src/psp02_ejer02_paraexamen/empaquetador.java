00c578f231a507fc75b365d0bfcbe397ecc62fc7
chern007 <carlos.hernandez_crespo@nokia>
2018-02-07 11:09:17 +0100

    terminado y funcionando los semaforos

--- src/psp02_ejer02_paraexamen/empaquetador.java
+++ src/psp02_ejer02_paraexamen/empaquetador.java
@@ -5,7 +5,11 @@
  */
 package psp02_ejer02_paraexamen;
 
+import static java.lang.Thread.sleep;
+import java.util.Random;
 import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -15,21 +19,47 @@ public class empaquetador implements Runnable {
 
     int[] cadena;
     Semaphore[] semaforos;
+    int numPaquete;
 
-    public empaquetador(int[] cadena, Semaphore[] semaforos) {
+    public empaquetador(int[] cadena, Semaphore[] semaforos, int numPaquete) {
         this.cadena = cadena;
         this.semaforos = semaforos;
+        this.numPaquete = numPaquete;
     }
 
     @Override
     public void run() {
-        
-        while(true){
-            
-            
+
+        while (true) {
+
+            //ESPERAMOS 1'5 SEGUNDOS
+            try {
+                sleep(1500);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(colocador.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            for (int i = 0; i < 10; i++) {
+
+                if (cadena[i] == numPaquete) { //si el paquete es del numero que esta especializado el empaquetador
+
+                    if (semaforos[i].tryAcquire()) {
+                        
+                        System.out.println("EMPAQUETADOR " + numPaquete + ": Se ha empaquetado el elemento " + i + " (" + cadena[i] + ")");
+                        cadena[i] = 0;//vaciamos el contenido
+                        semaforos[i].release();
+
+                    } else {
+                        System.out.println("EMPAQUETADOR: POS-" + i + " vacia. HILO--> " + Thread.currentThread().getName());
+
+                    }
+
+                }
+
+            }
+
         }
-        
-        
+
     }
 
 }
--- src/psp02_ejer02_paraexamen/main.java
+++ src/psp02_ejer02_paraexamen/main.java
@@ -11,23 +11,35 @@ import java.util.concurrent.Semaphore;
  *
  * @author Carlos
  */
-
 //fuente: https://resolviendoejerciciosdeprogramacion.blogspot.com.es/2014/06/ejercicio-concurrencia-con-semaforos-en.html
-
 public class main {
 
     /**
      * @param args the command line arguments
      */
-    
     public static int[] cadena = new int[10];
-    public static Semaphore[] semaforos= new Semaphore[10];
-    
+    public static Semaphore[] semaforos = new Semaphore[10];
 
-    
-    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
+        //rellenamos el array de semaforos
+        for (int i = 0; i < 10; i++) {
+            semaforos[i] = new Semaphore(1);
+        }       
+
+        //iniciamos los hilos
+        Thread hiloColocador = new Thread(new colocador(cadena, semaforos));
+        Thread hiloEmpaquetador1 = new Thread(new empaquetador(cadena, semaforos, 1));
+        Thread hiloEmpaquetador2 = new Thread(new empaquetador(cadena, semaforos, 2));
+        Thread hiloEmpaquetador3 = new Thread(new empaquetador(cadena, semaforos, 3));
+        
+        hiloColocador.start();
+        hiloEmpaquetador1.start();
+        hiloEmpaquetador2.start();
+        hiloEmpaquetador3.start();
+                        
+                        
     }
     
 }
